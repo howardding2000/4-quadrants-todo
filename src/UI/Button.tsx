71f0e43ebba464461efa5a6e3ef39cccdd7b4bb1
howard <howardding2000@gmail.com>
2022-02-17 18:56:43 -0500

    add touch events

--- src/UI/Button.tsx
+++ src/UI/Button.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 import classes from './Button.module.css';
 
 
 const Button: React.FC<any> = (props) => {
+    const [touching, isTouching] = useState(false);
 
-    return <button className={classes.button} onClick={props.onClick}>{props.children}</button>
+    const touchStartHandler = ()    =>{
+        isTouching(true);
+    }
+    const touchEndHandler = ()    =>{
+        isTouching(false);
+    }
+    return <button className={`${classes.button} ${touching && classes.touching}`} onClick={props.onClick} onTouchStart={touchStartHandler} onTouchEnd={touchEndHandler}>{props.children}</button>
 };
 
 export default Button;
