bfb7b1e718e5893c533946e75c7179e343f2588f
howard <howardding2000@gmail.com>
2022-02-19 17:25:58 -0500

    update touch style

--- src/UI/Button.tsx
+++ src/UI/Button.tsx
@@ -1,17 +1,12 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import classes from './Button.module.css';
 
-
 const Button: React.FC<any> = (props) => {
-    const [touching, isTouching] = useState(false);
-
-    const touchStartHandler = ()    =>{
-        isTouching(true);
-    }
-    const touchEndHandler = ()    =>{
-        isTouching(false);
-    }
-    return <button className={`${classes.button} ${touching && classes.touching}`} onClick={props.onClick} onTouchStart={touchStartHandler} onTouchEnd={touchEndHandler}>{props.children}</button>
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
